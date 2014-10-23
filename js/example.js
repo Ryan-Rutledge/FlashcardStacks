931bf2438fd7e19ade4895fd15fd786155ea1c10
Ryan-Rutledge <RyanARutledge@gmail.com>
2014-10-22 22:59:22 -0500

    Added example javascript

---
+++ js/example.js
@@ -0,0 +1,89 @@
+function example_init() {
+	// Load integer flashcards
+	for (var i in integers)
+		fc_stack.integers.push(
+			new fc_FlashCard(integers[i])
+		);
+
+	// Load color flashcards
+	for (var c in colors)
+		fc_stack.colors.push(
+			new fc_FlashCard(colors[c])
+		);
+}
+
+/******************************
+ * Integer class              *
+ ******************************/
+
+Integer = function(number, string) {
+	this.number = number;
+	this.string = string;
+}
+Integer.prototype.drawFront = function(ctx) {
+	var w = ctx.canvas.width / 2;
+	var h = ctx.canvas.height / 2;
+
+	// Circle
+	ctx.beginPath();
+	ctx.fillStyle = '#333';
+	ctx.arc(w, h, w / 2,0,2*Math.PI);
+	ctx.fill();
+	ctx.closePath();
+
+	// Number
+	ctx.fillStyle = '#fff';
+	ctx.font = w + 'px sans';
+	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
+	ctx.fillText(this.number, w, h);
+}
+Integer.prototype.drawBack = function(ctx) {
+	var w = ctx.canvas.width / 2;
+	var h = ctx.canvas.height / 2;
+
+	// Number
+	ctx.fillStyle = '#333';
+	ctx.font = w / 2 + 'px sans';
+	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
+	ctx.fillText(this.string, w, h);
+}
+
+/******************************
+ * Colors class               *
+ ******************************/
+
+Color = function(color, name) {
+	this.color = color;
+	this.name = name;
+}
+Color.prototype.drawFront = function(ctx) {
+	var w = ctx.canvas.width / 2;
+	var h = ctx.canvas.height / 2;
+
+	// Number
+	ctx.fillStyle = '#333';
+	ctx.font = w / 2 + 'px sans';
+	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
+	ctx.fillText(this.name, w, h);
+}
+Color.prototype.drawBack = function(ctx) {
+	ctx.fillStyle = this.color;
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+/******************************
+ * Initiate instances         *
+ ******************************/
+
+colors = [];
+colors.push(new Color('#F00', 'Red'));
+colors.push(new Color('#0F0', 'Green'));
+colors.push(new Color('#00F', 'Blue'));
+
+integers = [];
+integers.push(new Integer(1, 'One'));
+integers.push(new Integer(2, 'Two'));
+integers.push(new Integer(3, 'Three'));
