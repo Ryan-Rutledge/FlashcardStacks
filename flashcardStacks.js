6f7a6637787022af5601ab9d535bf83cbdac54a2
Ryan-Rutledge <RyanARutledge@gmail.com>
2014-11-16 17:14:03 -0600

    Improved code readability

--- flashcardStacks.js
+++ flashcardStacks.js
@@ -38,8 +38,8 @@ var fc = {
 	// Moves card based on cursor/pointer movement
 	touchend: function(e) {
 		if (fc.touchedStack) {
-			var endX = e.changedTouches[0].pageX;
-			var endY = e.changedTouches[0].pageY;
+			var endX = e.changedTouches[0].pageX,
+				endY = e.changedTouches[0].pageY;
 			e.preventDefault();
 
 			// If swipe length is long enough
@@ -57,24 +57,25 @@ var fc = {
 		fc.touchY = null;
 		fc.prevDir = null;
 
-		if (fc.touchedStack)
+		if (fc.touchedStack) {
 			with (fc.touchedStack) {
 				card.classList.remove('fc_tiltLeft');
 				card.classList.remove('fc_tiltRight');
 				outerHolder.classList.remove('fc_tiltUp');
 				outerHolder.classList.remove('fc_tiltDown');
 			}
+		}
 	},
 
 	// Tilts card based on mouse/pointer movement
 	touchmove: function(e) {
 		if (!this.isAnimating && fc.touchedStack) {
 			e.preventDefault();
-			var card = fc.touchedStack.card;
-			var holder = fc.touchedStack.outerHolder;
-			var curX = e.touches[0].pageX;
-			var curY = e.touches[0].pageY;
-			var dist = Math.max(Math.abs(curX - fc.touchX), Math.abs(curY - fc.touchY));
+			var card = fc.touchedStack.card,
+				holder = fc.touchedStack.outerHolder,
+				curX = e.touches[0].pageX,
+				curY = e.touches[0].pageY,
+				dist = Math.max(Math.abs(curX - fc.touchX), Math.abs(curY - fc.touchY));
 
 			// If length of swipe is long enough
 			if (dist > fc.touchedStack.swipeDist) {
@@ -139,14 +140,19 @@ var fc = {
 		fc.resetTouchEvent();
 
 		// Load parameter objects
-		for (var key in objectStacks)
-			if (fc.stacks[key].usingCanvas)
-				for (var o in objectStacks[key])
+		for (var key in objectStacks) {
+			if (fc.stacks[key].usingCanvas) {
+				for (var o in objectStacks[key]) {
 					fc.stacks[key].push(new fc.FlashCard(objectStacks[key][o]));
+				}
+			}
 			else 
-				for (var o in objectStacks[key])
-					if (objectStacks[key][o])
+				for (var o in objectStacks[key]) {
+					if (objectStacks[key][o]) {
 						fc.stacks[key].fc_cards[o].events = objectStacks[key][o];
+					}
+				}
+		}
 
 		// Get layout engine info
 		var LE = 'webkitTransform' in document.body.style ?  'webkit' :'MozTransform' in document.body.style ?  'Moz':'Ms' in document.body.style ?  'Ms':'';
@@ -159,30 +165,33 @@ var fc = {
 			fc.Stack.prototype.flipCard = function(direction) {
 				if (!this.isAnimating && !(this.container.getAttribute('fc-disableFlip') === 'true')) {
 					this.isAnimating = true;
-					var thisStack = this
+					var thisStack = this,
+						faceArr = ['fc_faceup', 'fc_facedown'],
+						dirArr = ['fc_flipLeft', 'fc_flipRight'];
 
 					// Remove tilt classes
 					thisStack.card.classList.remove('fc_animateTilt');
 					thisStack.outerHolder.classList.remove('fc_animateTilt');
 
-					// Switch facedown and faceup css classes after animation finishes
-					var faceArr = ['fc_faceup', 'fc_facedown'];
-
-					if (this.isFaceUp) faceArr.reverse();
+					if (this.isFaceUp) {
+						faceArr.reverse();
+					}
 					this.isFaceUp = !this.isFaceUp;
 
-					var t1 = setTimeout(function() {
+					// Switch facedown and faceup css classes after animation finishes
+					setTimeout(function() {
 						thisStack.card.classList.add(faceArr[0]);
 						thisStack.card.classList.remove(faceArr[1]);
 					}, fc.FLIP_TIME);
 
-					// Add css flip animation classes, then remove them after animation finishes
-					var dirArr = ['fc_flipLeft', 'fc_flipRight'];
 
-					if (direction == fc.MOVEMENT.RIGHT) dirArr.reverse();
+					if (direction == fc.MOVEMENT.RIGHT) {
+						dirArr.reverse();
+					}
 					
+					// Temporarily add css flip animation classes
 					thisStack.card.classList.add(dirArr[0]);
-					var t2 = setTimeout(function() {
+					setTimeout(function() {
 						thisStack.card.classList.remove(dirArr[0]);
 						thisStack.isAnimating = false;
 					}, fc.FLIP_TIME);
@@ -207,10 +216,11 @@ var fc = {
 						case fc.MOVEMENT.UP:
 							thisStack.outerHolder.classList.add('fc_moveUp');
 
-							var t1 = setTimeout(function() {
+							setTimeout(function() {
 								thisStack.showNextCard();
 							}, fc.SWITCH_TIME/2);
-							var t2 = setTimeout(function() {
+
+							setTimeout(function() {
 								thisStack.outerHolder.classList.remove('fc_moveUp');
 								thisStack.isAnimating = false;
 							}, fc.SWITCH_TIME);
@@ -219,11 +229,11 @@ var fc = {
 						default:
 							thisStack.outerHolder.classList.add('fc_moveDown');
 
-							var t1 = setTimeout(function() {
+							setTimeout(function() {
 								thisStack.showPrevCard(thisStack);
 							}, fc.SWITCH_TIME/2);
 
-							var t2 = setTimeout(function() {
+							setTimeout(function() {
 								thisStack.outerHolder.classList.remove('fc_moveDown');
 								thisStack.isAnimating = false;
 							}, fc.SWITCH_TIME);
@@ -251,10 +261,12 @@ var fc = {
 				if (!(this.container.getAttribute('fc-disableSwitch') === 'true')) {
 					this.handleSwitch(this, fc.MOVEMENT.LEAVE);
 
-					if (direction === fc.MOVEMENT.UP)
+					if (direction === fc.MOVEMENT.UP) {
 						this.showNextCard();
-					else
+					}
+					else {
 						this.showPrevCard();
+					}
 				}
 			}
 		}
@@ -282,8 +294,9 @@ var fc = {
 
 			// Set mouse drag listener for stacks
 			drag: function(stacks) {
-				for (var s in stacks)
+				for (var s in stacks) {
 					setListener.dragStack(stacks[s]);
+				}
 
 				// MouseUp
 				window.addEventListener('mouseup', function(e) {
@@ -297,8 +310,9 @@ var fc = {
 
 			// Set click listener for stacks
 			click: function(stacks) {
-				for (var s in stacks)
+				for (var s in stacks) {
 					setListener.clickStack(stacks[s]);
+				}
 			},
 
 			// Set click listener for individual stack
@@ -320,12 +334,16 @@ var fc = {
 
 			// Set touch listeners for stacks
 			swipe: function(stacks) {
-				for (var s in stacks)
+				for (var s in stacks) {
 					setListener.swipeStack(stacks[s]);
+				}
 
 				// Touchend
 				window.addEventListener('touchend', function(e) {
-					if (fc.touchedStack) fc.touchend(e);
+					if (fc.touchedStack) {
+						fc.touchend(e);
+					}
+
 					fc.touchedStack = false;
 				});
 			},
@@ -342,19 +360,35 @@ var fc = {
 			// Set keydown listener for stacks
 			arrowkeys: function(stacks) {
 					window.addEventListener('keydown', function(e) {
-						if (e.which >= 37 && e.which <= 40)
-							for (var s in stacks)
+						if (e.which >= 37 && e.which <= 40) {
+							for (var s in stacks) {
 								stacks[s].moveCard(e.which - 37);
+							}
+						}
 					}, false);
 			}
 		};
 
-		// Add listeners
-		if (fc.animationIsSupported && fc.tiltStacks.length) setListener.tilt();
-		if (fc.dragStacks.length) setListener.drag(fc.dragStacks);
-		if (fc.clickStacks.length) setListener.click(fc.clickStacks);
-		if (fc.swipeStacks.length) setListener.swipe(fc.swipeStacks);
-		if (fc.arrowkeyStacks.length) setListener.arrowkeys(fc.arrowkeyStacks);
+		// Tilt listeners
+		if (fc.animationIsSupported && fc.tiltStacks.length) {
+			setListener.tilt();
+		}
+		// Drag listeners
+		if (fc.dragStacks.length) {
+			setListener.drag(fc.dragStacks);
+		}
+		// Click listeners
+		if (fc.clickStacks.length) {
+			setListener.click(fc.clickStacks);
+		}
+		// Swipe listeners
+		if (fc.swipeStacks.length) {
+			setListener.swipe(fc.swipeStacks);
+		}
+		// Arrow key listeners
+		if (fc.arrowkeyStacks.length) {
+			setListener.arrowkeys(fc.arrowkeyStacks);
+		}
 	}
 }
 
@@ -377,10 +411,12 @@ fc.FlashCard = function() {
 fc.FlashCard.prototype.draw = function(front, back) {
 	with (this) {
 		if (events) {
-			if (events.drawFront)
+			if (events.drawFront) {
 				drawFront(front.getContext('2d'));
-			if (events.drawBack)
+			}
+			if (events.drawBack) {
 				drawBack(back.getContext('2d'));
+			}
 		}
 	}
 }
@@ -403,29 +439,33 @@ fc.FlashCard.prototype.drawBack = function(ctx) {
 fc.FlashCard.prototype.handleFlip = function(stack, direction) {
 	self = this;
 
-	if (self.events.onFlip)
+	if (self.events.onFlip) {
 		self.events.onFlip(stack);
+	}
 
 	if (stack.isFaceUp) {
-		if (self.events.onFlipUp)
+		if (self.events.onFlipUp) {
 			self.events.onFlipUp(stack);
+		}
 	}
 	else if (self.events.onFlipDown) {
 		self.events.onFlipDown(stack);
 	}
 
 	if (direction === fc.MOVEMENT.RIGHT) {
-		if (self.events.onFlipRight)
+		if (self.events.onFlipRight) {
 			self.events.onFlipRight(stack);
+		}
 	}
 	else if (self.events.onFlipLeft) {
 		self.events.onFlipLeft(stack);
 	}
 
 	if (self.events.onFlipFinish) {
-		t = setTimeout(function() {
-			if (self.events.onFlipFinish)
+		setTimeout(function() {
+			if (self.events.onFlipFinish) {
 				self.events.onFlipFinish(stack);
+			}
 		}, fc.FLIP_TIME);
 	}
 }
@@ -435,21 +475,25 @@ fc.FlashCard.prototype.handleSwitch = function(stack, movement) {
 	var self = this;
 
 	if (movement === fc.MOVEMENT.LEAVE) {
-		if (self.events.onSwitch)
+		if (self.events.onSwitch) {
 			self.events.onSwitch(stack);
+		}
 
-		if (self.events.onLeave)
+		if (self.events.onLeave) {
 			self.events.onLeave(stack);
+		}
 	}
 	else {
-		if (self.events.onEnter)
+		if (self.events.onEnter) {
 			self.events.onEnter(stack);
+		}
 			
 		if (self.events.onSwitchFinish) {
 			if (fc.animationIsSupported) {
-				t = setTimeout(function() {
-					if (self.events.onSwitchFinish)
+				setTimeout(function() {
+					if (self.events.onSwitchFinish) {
 						self.events.onSwitchFinish(stack);
+					}
 				}, fc.SWITCH_TIME / 2);
 			}
 			else {
@@ -482,15 +526,30 @@ fc.Stack = function(container) {
 	self.enabled.arrowkeys = container.getAttribute('fc-enableArrowkeys') === '';
 
 	// If no fc attributes are provided, enable everything
-	if (!(self.enabled.tilt || self.enabled.drag || self.enabled.click || self.enabled.swipe || self.enabled.arrowkeys))
-		self.enabled.tilt = self.enabled.drag = self.enabled.click = self.enabled.swipe = self.enabled.arrowkeys = true;
+	if (!(self.enabled.tilt || self.enabled.drag || self.enabled.click || self.enabled.swipe || self.enabled.arrowkeys)) {
+		self.enabled.tilt =
+		self.enabled.drag =
+		self.enabled.click =
+		self.enabled.swipe =
+		self.enabled.arrowkeys = true;
+	}
 
-	// Add self stack to appropriate arrays
-	if (self.enabled.tilt) fc.tiltStacks.push(self);
-	if (self.enabled.drag) fc.dragStacks.push(self);
-	if (self.enabled.click) fc.clickStacks.push(self);
-	if (self.enabled.swipe) fc.swipeStacks.push(self);
-	if (self.enabled.arrowkeys) fc.arrowkeyStacks.push(self);
+	// Add stack to appropriate arrays
+	if (self.enabled.tilt) {
+		fc.tiltStacks.push(self);
+	}
+	if (self.enabled.drag) {
+		fc.dragStacks.push(self);
+	}
+	if (self.enabled.click) {
+		fc.clickStacks.push(self);
+	}
+	if (self.enabled.swipe) {
+		fc.swipeStacks.push(self);
+	}
+	if (self.enabled.arrowkeys) {
+		fc.arrowkeyStacks.push(self);
+	}
 
 	self.enabled.scaling = container.getAttribute('fc-enableCanvasScale') === '';
 
@@ -539,20 +598,33 @@ fc.Stack = function(container) {
 	container.appendChild(self.outerHolder);
 
 	// Set dimensions
-	if (container.getAttribute('fc-margin')) self.outerHolder.style.padding = container.getAttribute('fc-margin');
-	self.front.height = self.back.height = container.getAttribute('fc-height') ? container.getAttribute('fc-height'):400;
-	self.front.width = self.back.width = container.getAttribute('fc-width') ? container.getAttribute('fc-width'):600;
+	if (container.getAttribute('fc-margin')) {
+			self.outerHolder.style.padding = container.getAttribute('fc-margin');
+	}
+
+	// Set side heights
+	self.front.height =
+	self.back.height =
+	container.getAttribute('fc-height') ? container.getAttribute('fc-height'):400;
+
+	// Set side widths
+	self.front.width =
+	self.back.width =
+	container.getAttribute('fc-width') ? container.getAttribute('fc-width'):600;
+
+	// Set card height and width
 	self.card.style.height = self.front.height + 'px';
 	self.card.style.width = self.front.width + 'px';
 	self.aspectRatio = self.front.width / self.front.height;
 
 	if (!self.usingCanvas) {
-		for (var count = Math.floor(elements.length / 2); count > 0; count--) {
-			var front = container.removeChild(elements[0]);
-			var back = container.removeChild(elements[0]);
-			
-			var flashcard = new fc.FlashCard(front, back)
+		var count = Math.floor(elements.length / 2)
+		for (; count > 0; count--) {
+			var front = container.removeChild(elements[0]),
+				back = container.removeChild(elements[0]),
+				flashcard = new fc.FlashCard(front, back);
 
+			// Add individual card events
 			flashcard.events.onSwitch = window[front.getAttribute('fc-onSwitch')];
 			flashcard.events.onEnter = window[front.getAttribute('fc-onEnter')];
 			flashcard.events.onLeave = window[front.getAttribute('fc-onLeave')];
@@ -589,10 +661,12 @@ fc.Stack.prototype.push = function(flashcard) {
 
 // Pop card from stack
 fc.Stack.prototype.pop = function() {
-	if (this.fc_cards.length <= 1)
+	if (this.fc_cards.length <= 1) {
 		this.outerHolder.style.display = 'none';
-	else if (this.cur === this.fc_cards.length - 1)
+	}
+	else if (this.cur === this.fc_cards.length - 1) {
 		this.showPrevCard();
+	}
 
 	return this.fc_cards.pop();
 }
@@ -606,16 +680,20 @@ fc.Stack.prototype.load = function() {
 };
 
 // Returns currently displayed card
-fc.Stack.prototype.curCard = function() {return this.fc_cards[this.cur]};
+fc.Stack.prototype.curCard = function() {
+	return this.fc_cards[this.cur]
+};
 
 // Switch to, and draw, the previous card
 fc.Stack.prototype.showPrevCard = function() {
 	this.cur = (this.cur + this.fc_cards.length - 1) % this.fc_cards.length;
 
-	if (this.usingCanvas)
+	if (this.usingCanvas) {
 		this.draw();
-	else
+	}
+	else {
 		this.load();
+	}
 
 	this.handleSwitch(this, fc.MOVEMENT.ENTER);
 };
@@ -624,10 +702,12 @@ fc.Stack.prototype.showPrevCard = function() {
 fc.Stack.prototype.showNextCard = function() {
 	this.cur = (this.cur + 1) % this.fc_cards.length;
 
-	if (this.usingCanvas)
+	if (this.usingCanvas) {
 		this.draw();
-	else
+	}
+	else {
 		this.load();
+	}
 
 	this.handleSwitch(this, fc.MOVEMENT.ENTER);
 };
@@ -636,12 +716,14 @@ fc.Stack.prototype.showNextCard = function() {
 fc.Stack.prototype.resize = function() {
 	with (this) {
 		var h = innerHolder.clientHeight;
-		var w = innerHolder.clientWidth;
+			w = innerHolder.clientWidth;
 
-		if (h*aspectRatio > w)
+		if (h*aspectRatio > w) {
 			h = w/aspectRatio;
-		else
+		}
+		else {
 			w = h*aspectRatio;
+		}
 
 		card.style.maxHeight = h + 'px';
 		card.style.maxWidth = w + 'px';
@@ -661,20 +743,23 @@ fc.Stack.prototype.handleFlip = function(direction) {
 	var self = this;
 	self.curCard().handleFlip(self, direction);
 
-	if (window[self.container.getAttribute('fc-onFlip')])
+	if (window[self.container.getAttribute('fc-onFlip')]) {
 		window[self.container.getAttribute('fc-onFlip')](self);
+	}
 
 	if (self.isFaceUp) {
-		if (window[self.container.getAttribute('fc-onFlipUp')])
+		if (window[self.container.getAttribute('fc-onFlipUp')]) {
 			window[self.container.getAttribute('fc-onFlipUp')](self);
+		}
 	}
 	else if (window[self.container.getAttribute('fc-onFlipDown')]) {
 		self.window[self.container.getAttribute('fc-onFlipDown')](self);
 	}
 
 	if (direction === fc.MOVEMENT.RIGHT) {
-		if (window[self.container.getAttribute('fc-onFlipRight')])
+		if (window[self.container.getAttribute('fc-onFlipRight')]) {
 			window[self.container.getAttribute('fc-onFlipRight')](self);
+		}
 	}
 	else if (window[self.container.getAttribute('fc-onFlipLeft')]) {
 		window[self.container.getAttribute('fc-onFlipLeft')](self);
@@ -682,9 +767,10 @@ fc.Stack.prototype.handleFlip = function(direction) {
 
 	if (window[self.container.getAttribute('fc-onFlipFinish')]) {
 		if (fc.animationIsSupported) {
-			t = setTimeout(function() {
-				if (window[self.container.getAttribute('fc-onFlipFinish')])
+			setTimeout(function() {
+				if (window[self.container.getAttribute('fc-onFlipFinish')]) {
 					window[self.container.getAttribute('fc-onFlipFinish')](self);
+				}
 			}, fc.FLIP_TIME);
 		}
 		else {
@@ -699,21 +785,25 @@ fc.Stack.prototype.handleSwitch = function(movement) {
 	self.curCard().handleSwitch(self, movement);
 
 	if (movement === fc.MOVEMENT.LEAVE) {
-		if (window[self.container.getAttribute('fc-onSwitch')])
+		if (window[self.container.getAttribute('fc-onSwitch')]) {
 			window[self.container.getAttribute('fc-onSwitch')](self);
+		}
 
-		if (window[self.container.getAttribute('fc-onLeave')])
+		if (window[self.container.getAttribute('fc-onLeave')]) {
 			window[self.container.getAttribute('fc-onLeave')](self)
+		}
 	}
 	else {
-		if (window[self.container.getAttribute('fc-onEnter')])
+		if (window[self.container.getAttribute('fc-onEnter')]) {
 			window[self.container.getAttribute('fc-onEnter')](self);
+		}
 		
 		if (window[self.container.getAttribute('fc-onSwitchFinish')]) {
 			if (fc.animationIsSupported) {
-				t = setTimeout(function() {
-					if (window[self.container.getAttribute('fc-onSwitchFinish')])
+				setTimeout(function() {
+					if (window[self.container.getAttribute('fc-onSwitchFinish')]) {
 						window[self.container.getAttribute('fc-onSwitchFinish')](self);
+					}
 				}, fc.SWITCH_TIME / 2);
 			}
 			else {
@@ -740,5 +830,7 @@ fc.Stack.prototype.moveCard = function(direction) {
 
 // Draw front and back of current card
 fc.Stack.prototype.draw = function() {
-	if (this.fc_cards.length) this.curCard().draw(this.front, this.back);
+	if (this.fc_cards.length) {
+		this.curCard().draw(this.front, this.back);
+	}
 }
